b179ff73c8cb7a85b368ad72a2b278a5c8f72acc
liweiping <18819446836@163.com>
2021-02-05 20:20:30 +0800

    提交知识

--- learnkotlin/src/main/java/com/learn/kotlin/day01/KnowledgePoints02.kt
+++ learnkotlin/src/main/java/com/learn/kotlin/day01/KnowledgePoints02.kt
@@ -3,5 +3,13 @@ package com.learn.kotlin.day01
 /**
  * @author shuimu{lwp}
  * @time 2019/8/14  11:59
- * @desc
+ * @desc 资料来源：https://www.cnblogs.com/Jetictors/p/8031291.html
  */
+ /**
+     * kotlin的修饰符
+     * public:所有地方可见。当没有声明任何修饰符时，kotlin默认使用该修饰符。
+     * internal:模块中可见。
+     */
+class KnowledgePoints02 {
+    
+}
