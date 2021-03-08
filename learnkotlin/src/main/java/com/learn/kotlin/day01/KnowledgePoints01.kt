98472e38884740d51331ece04ae809b42f8af193
liweiping <18819446836@163.com>
2021-03-08 23:26:06 +0800

    kotlin 补充 平台类型的特点 补充 拓展函数对于 类存在泛型的的情况下,因为泛型擦除的影响 补充 分散运算符

--- learnkotlin/src/main/java/com/learn/kotlin/day01/KnowledgePoints01.kt
+++ learnkotlin/src/main/java/com/learn/kotlin/day01/KnowledgePoints01.kt
@@ -112,6 +112,9 @@ import com.learn.kotlin.bean.Student
  * as?-->安全转换运算符。用于进行类型强制转换时,类型不合适,强转失败的情况下,返回一个null值。如
  * person as? com.learn.kotlin.bean.Student;该行代表表示:尝试强制转换person对象的类型为Student类型,如果成功则返回该对象的student类型,如果强转失败,则返回null。
  *
+ * "*"星符号-->分散运算符。用于把kotlin的数组中的元素分散开一个个进行 传递。如,声明 fun test(vararg element:String){代码};因为test函数的参数是一个可变参数,当
+ * 我们把数组Array<String>类型的对象传递给test()函数时,直接传递是不行的,必须在Array<String>前面+"*"星符合,代表打散Array<String>数组,把数组中的元素一个个传递进去。
+ *
  * is-->类型检查运算符。用于判断对象是否是某个类型的实例,相当于Java中的 instance of。 !is表示对象是否不是某个类型的实例。当我们使用is关键字判断了
  * 对于kotlin来说,当对象通过了is关键字的判断代表-->如果判断结果为true,说明该对象就是这个类型,那就没必要进行强制转换了,kotlin会自动进行智能转换,不需要手动进行强制转换了
  *
@@ -151,6 +154,10 @@ import com.learn.kotlin.bean.Student
  * 这些原始的数组类型，没有装箱的开销
  * 其他的类型：Array<Type>
  * 通过对应的数组方法创建数组。长度不可变，除了创建方式与java不同之外,其他特性是相同的。
+ * kotlin中的这些原始数组类型所创建的数组对象在面对JVM平台时,将会转换成对应类型的数组。
+ * 例如:IntArray数组对象在jvm上就会被转换成int[]数组。
+ * Array<Type>在jvm上将会转换成 java中任意类型的数组-->Array<String>==String[],Array<Person>==Person[]
+ *
  */
 class KnowledgePoints01 {
     val name: String by lazy {
